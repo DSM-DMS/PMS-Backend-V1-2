abbf413027c65050e4043109d0cc641f65e8c922
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-20 15:38:46 +0900

    [ADD] database connection option setting

---
+++ ormconfig.ts
@@ -0,0 +1,44 @@
+import "dotenv/config";
+import { ConnectionOptions } from "typeorm";
+
+interface DBConnectionOptions {
+  [env: string]: ConnectionOptions;
+}
+
+const connectionOptions: DBConnectionOptions = {
+  test: {
+    type: "mysql",
+    host: process.env.TEST_DATABASE_HOST,
+    port: +process.env.TEST_DATABASE_PORT,
+    username: process.env.TEST_DATABASE_USER,
+    password: process.env.TEST_DATABASE_PASSWORD,
+    database: process.env.TEST_DATABASE_NAME,
+    synchronize: true,
+    logging: false,
+    entities: ["./dist/**/*.entity.js"],
+  },
+  development: {
+    type: "mysql",
+    host: process.env.DEVELOPMENT_DATABASE_HOST,
+    port: +process.env.DEVELOPMENT_DATABASE_PORT,
+    username: process.env.DEVELOPMENT_DATABASE_USER,
+    password: process.env.DEVELOPMENT_DATABASE_PASSWORD,
+    database: process.env.DEVELOPMENT_DATABASE_NAME,
+    synchronize: true,
+    logging: true,
+    entities: ["./dist/**/*.entity.js"],
+  },
+  production: {
+    type: "mysql",
+    host: process.env.PRODUCTION_DATABASE_HOST,
+    port: +process.env.PRODUCTION_DATABASE_PORT,
+    username: process.env.PRODUCTION_DATABASE_USER,
+    password: process.env.PRODUCTION_DATABASE_PASSWORD,
+    database: process.env.PRODUCTION_DATABASE_NAME,
+    synchronize: false,
+    logging: true,
+    entities: ["./dist/**/*.entity.js"],
+  }
+}
+
+export { connectionOptions };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV])],
   controllers: [AppController],
   providers: [AppService],
 })
