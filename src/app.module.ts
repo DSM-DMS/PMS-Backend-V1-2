65d6bb1f2274d47221030e3b9a3f56e24b716937
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-23 09:28:45 +0900

    [MODIFY] app module
    adapt code format

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,20 @@ import { connectionOptions } from "ormconfig";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { HttpErrorFilter } from "./shared/http-error.filter";
+import { EventModule } from "./event/event.module";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV])],
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV]),
+    EventModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_FILTER,
-    useClass: HttpErrorFilter,
-  }],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import "dotenv/config";
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
 
 const port: string = process.env.PORT || "3000";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(port);
   Logger.log(`server on ${port}`, "Bootstrap");
 }
