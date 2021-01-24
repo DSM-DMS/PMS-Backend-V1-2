aba7432fcaba8d645f156a3827cae5f6554dcbeb
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-24 21:29:49 +0900

    [REMOVE] app controller, service
    not use / path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from "@nestjs/common";
 import { APP_FILTER } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { connectionOptions } from "ormconfig";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { HttpErrorFilter } from "./shared/http-error.filter";
 import { EventModule } from "./event/event.module";
 
@@ -12,9 +10,7 @@ import { EventModule } from "./event/event.module";
     TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV]),
     EventModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_FILTER,
       useClass: HttpErrorFilter,
