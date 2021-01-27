e494f50b92ecc2be28e02818440a8416d8e2183a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-28 06:26:47 +0900

    [ADD] authentication decorator
    토큰 인증 후 email을 받는 데코레이터 추가

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "../shared/authentication/auth.guard";
 import { EventService } from './event.service';
 
 @Controller("event")
@@ -6,6 +7,7 @@ export class EventController {
   constructor(private eventService: EventService) {}
 
   @Get("/meal/picture/:datetime")
+  @UseGuards(new AuthGuard())
   showMealPictures(@Param("datetime") datetime: string) {
     return this.eventService.getPicturesOnTheDay(datetime);
   }
