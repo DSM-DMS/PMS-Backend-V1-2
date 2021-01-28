bc309ab347c884f8d15b67b07e104fc791beff54
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-28 20:32:31 +0900

    [ADD] event meal picture controller

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { Auth } from "src/shared/authentication/auth.decorator";
 import { AuthGuard } from "../shared/authentication/auth.guard";
 import { EventService } from './event.service';
+import { multerUploadOption } from "./event.type";
+import { UploadPictureDto } from "./meal/meal.dto";
 
 @Controller("event")
 export class EventController {
@@ -10,5 +14,15 @@ export class EventController {
   @UseGuards(new AuthGuard())
   showMealPictures(@Param("datetime") datetime: string) {
     return this.eventService.getPicturesOnTheDay(datetime);
+  }
+
+  @Post("/meal/picture")
+  @UseGuards(new AuthGuard())
+  @UseInterceptors(FileInterceptor("file", multerUploadOption))
+  uploadMealPicture(
+    @UploadedFile() file: Express.Multer.File, 
+    @Auth("email") email: string, 
+    @Body() body: UploadPictureDto) {
+    return this.uploadMealPicture(file, email, body);
   }
 }
