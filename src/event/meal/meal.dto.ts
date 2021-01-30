8bcc9c84361335caf933cdb44e52ed4dc590191f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-30 11:45:44 +0900

    [ADD] meal list dto

--- src/event/meal/meal.dto.ts
+++ src/event/meal/meal.dto.ts
@@ -1,7 +1,15 @@
+import { Meal } from "./entity/meal.entity";
+
 export class MealCrawlDto {
   date: string;
   url: string;
   time?: string;
+}
+
+export class MealListDto {
+  date: string;
+  list: string;
+  time: keyof Meal;
 }
 
 export class MealResponseData {
