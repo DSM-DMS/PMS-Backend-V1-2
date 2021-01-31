970b5241fd0ddd078f5623124bc3a4b2ef5b7032
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-01 00:15:26 +0900

    [FIX] get calendar factory scheduler
    학교 일정을 받아오는 함수의 실행 컨텍스트를 유지

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CalendarDataFactory } from './calendar';
 import { Calendar } from './calendar.type';
+import * as schedule from "node-schedule";
 
 @Injectable()
 export class CalendarService {
   private calendar: CalendarDataFactory;
   constructor() {
     this.calendar = new CalendarDataFactory();
+    schedule.scheduleJob("0 0 0 1 * *", this.calendar.setCalendar.bind(this.calendar));
   }
 
   public async getCalendar(): Promise<Calendar> {
