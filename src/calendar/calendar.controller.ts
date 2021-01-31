e5e39a3faf79764dd8d3d2b5b628b8354617703e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-31 09:10:01 +0900

    [ADD] get calendar service
    학교 일정을 알려주는 api

--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CalendarService } from './calendar.service';
 
 @Controller('calendar')
-export class CalendarController {}
+export class CalendarController {
+  constructor(private calendarService: CalendarService) {}
+
+  @Get()
+  getCalendar() {
+    return this.calendarService.getCalendar();
+  }
+}
--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CalendarDataFactory } from './calendar';
+import { Calendar } from './calendar.type';
 
 @Injectable()
-export class CalendarService {}
+export class CalendarService {
+  private calendar: CalendarDataFactory;
+  constructor() {
+    this.calendar = new CalendarDataFactory();
+  }
+
+  public async getCalendar(): Promise<Calendar> {
+    return await this.calendar.getCalender();
+  }
+}
