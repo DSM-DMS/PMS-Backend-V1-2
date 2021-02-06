4e8bbfd1878b9833932502beac0dc3cdf0b9aa20
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-07 02:45:50 +0900

    [MODIFY] setting postgre sql

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -29,7 +29,7 @@ const connectionOptions: DBConnectionOptions = {
     entities: ["./dist/**/*.entity.js"],
   },
   production: {
-    type: "mysql",
+    type: "postgres",
     host: process.env.PRODUCTION_DATABASE_HOST,
     port: +process.env.PRODUCTION_DATABASE_PORT,
     username: process.env.PRODUCTION_DATABASE_USER,
