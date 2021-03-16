d1e33e201ca620c2fb88c93128e5f45092742df5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-16 16:36:13 +0900

    [MODIFY] event.controller.ts
    swagger api response, consumes 추가

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Get, Param, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
-import { ApiBody, ApiConsumes, ApiHeader, ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiConsumes, ApiHeader, ApiTags, ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
 import { Auth } from "../shared/authentication/auth.decorator";
 import { AuthGuard } from "../shared/authentication/auth.guard";
 import { EventService } from './event.service';
 import { multerUploadOption } from "./event.type";
-import { UploadPictureDto, UploadPictureResponseData } from "./meal/meal.dto";
+import { MealResponseData, UploadPictureDto, UploadPictureResponseData } from "./meal/meal.dto";
 
 @ApiTags("event")
+@ApiBearerAuth()
 @ApiHeader({ name: "Authorization", required: true })
 @Controller("event")
 export class EventController {
@@ -15,13 +16,16 @@ export class EventController {
 
   @Get("/meal/picture/:datetime")
   @UseGuards(new AuthGuard())
-  showMealPictures(@Param("datetime") datetime: string) {
+  @ApiResponse({ status: 200, type: MealResponseData })
+  @ApiConsumes("application/json")
+  showMealPictures(@Param("datetime") datetime: string): Promise<MealResponseData> {
     return this.eventService.getPicturesOnTheDay(datetime);
   }
 
   @Post("/meal/picture")
   @UseGuards(new AuthGuard())
   @UseInterceptors(FileInterceptor("file", multerUploadOption))
+  @ApiResponse({ status: 201, type: UploadPictureResponseData })
   @ApiConsumes("multipart/form-data")
   @ApiBody({ type: UploadPictureDto })
   uploadMealPicture(
@@ -32,8 +36,10 @@ export class EventController {
   }
 
   @Get("/meal/:datetime")
+  @ApiResponse({ status: 200, type: MealResponseData })
+  @ApiConsumes("application/json")
   @UseGuards(new AuthGuard())
-  async showMealLists(@Param("datetime") datetime: string) {
+  async showMealLists(@Param("datetime") datetime: string): Promise<MealResponseData> {
     return this.eventService.getMealListsOnTheDay(datetime);
   }
 }
