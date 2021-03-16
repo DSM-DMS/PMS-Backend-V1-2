ceeddc119c49908100cd2837eaf66bb45bc4650e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-16 18:59:25 +0900

    [MODIFY] event.controller.ts
    api response status code set

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Param, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
-import { ApiBody, ApiConsumes, ApiHeader, ApiTags, ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
+import { ApiBody, ApiConsumes, ApiHeader, ApiTags, ApiBearerAuth, ApiResponse, ApiOperation } from "@nestjs/swagger";
 import { Auth } from "../shared/authentication/auth.decorator";
 import { AuthGuard } from "../shared/authentication/auth.guard";
 import { EventService } from './event.service';
@@ -16,7 +16,10 @@ export class EventController {
 
   @Get("/meal/picture/:datetime")
   @UseGuards(new AuthGuard())
-  @ApiResponse({ status: 200, type: MealResponseData })
+  @ApiOperation({ summary: "급식 사진 API",  description: "급식 사진 목록을 객체로 반환" })
+    @ApiResponse({ status: 200, type: MealResponseData })
+    @ApiResponse({ status: 401, description: "인증 정보가 유효하지 않음" })
+    @ApiResponse({ status: 400, description: "급식 목록이 없음" })
   @ApiConsumes("application/json")
   showMealPictures(@Param("datetime") datetime: string): Promise<MealResponseData> {
     return this.eventService.getPicturesOnTheDay(datetime);
@@ -25,7 +28,11 @@ export class EventController {
   @Post("/meal/picture")
   @UseGuards(new AuthGuard())
   @UseInterceptors(FileInterceptor("file", multerUploadOption))
-  @ApiResponse({ status: 201, type: UploadPictureResponseData })
+  @ApiOperation({ summary: "급식 사진 업로드 API", description: "성공 시 상태 코드 201 반환" })
+    @ApiResponse({ status: 201, type: UploadPictureResponseData })
+    @ApiResponse({ status: 400, description: "잘못된 요청. 요청 값 확인" })
+    @ApiResponse({ status: 401, description: "인증 정보가 유효하지 않음" })
+    @ApiResponse({ status: 403, description: "접근 권한 없음" })
   @ApiConsumes("multipart/form-data")
   @ApiBody({ type: UploadPictureDto })
   uploadMealPicture(
@@ -36,9 +43,11 @@ export class EventController {
   }
 
   @Get("/meal/:datetime")
-  @ApiResponse({ status: 200, type: MealResponseData })
-  @ApiConsumes("application/json")
   @UseGuards(new AuthGuard())
+  @ApiOperation({ summary: "급식 목록 API",  description: "급식 목록을 객체로 반환" })
+    @ApiResponse({ status: 200, type: MealResponseData })
+    @ApiResponse({ status: 401, description: "인증 정보가 유효하지 않음" })
+  @ApiConsumes("application/json")
   async showMealLists(@Param("datetime") datetime: string): Promise<MealResponseData> {
     return this.eventService.getMealListsOnTheDay(datetime);
   }
