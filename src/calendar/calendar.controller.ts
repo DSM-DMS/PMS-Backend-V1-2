9b924703ac2e9a7c0e881c1e17d55185c2e83af7
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-16 16:48:26 +0900

    [MODIFY] calendar.controller.ts
    swagger api response, consumes 추가

--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiHeader, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '../shared/authentication/auth.guard';
-import { CalendarService } from './calendar.service';
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { ApiHeader, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "../shared/authentication/auth.guard";
+import { CalendarService } from "./calendar.service";
+import { ApiResponse, ApiConsumes } from "@nestjs/swagger";
 
 @ApiTags("calendar")
 @ApiHeader({ name: "Authorization", required: true })
@@ -11,6 +12,8 @@ export class CalendarController {
 
   @Get()
   @UseGuards(new AuthGuard())
+  @ApiResponse({ status: 200 })
+  @ApiConsumes("application/json")
   getCalendar() {
     return this.calendarService.getCalendar();
   }
