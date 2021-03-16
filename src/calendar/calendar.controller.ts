6ee31493c77ec8393b63506c2df32715064840b6
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-16 12:12:41 +0900

    [MODIFY] calendar.controller.ts
    swagger api 적용

--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiHeader, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '../shared/authentication/auth.guard';
 import { CalendarService } from './calendar.service';
 
+@ApiTags("calendar")
+@ApiHeader({ name: "Authorization", required: true })
 @Controller('calendar')
 export class CalendarController {
   constructor(private calendarService: CalendarService) {}
