115147c46529787b0a3b78908535bb64a2372fec
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-22 16:40:44 +0900

    [ADD] club.dto.ts swagger api
    club info res obj

---
+++ src/introduce/club/club.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class ClubInfoResObj {
+  @ApiProperty({ example: "DMS" })
+  title: string;
+
+  @ApiProperty({ example: "https://www.dsm-dms.com/example.jpg" })
+  uri: string;
+
+  @ApiProperty({ example: "기숙사 지원 시스템 개발동아리" })
+  explanation: string;
+
+  @ApiProperty({ example: "['Mary', 'Frank', 'Henry']" })
+  member?: string[];
+}
