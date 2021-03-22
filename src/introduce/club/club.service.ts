8d03aeaa01bec6c2d90390f9152f69739aeda669
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-22 22:16:23 +0900

    [MODIFY] <club.service.ts> Add get club info method

--- src/introduce/club/club.service.ts
+++ src/introduce/club/club.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ClubInfoResObj } from './club.dto';
+import { ClubRepository } from './entity/club.repository';
 
 @Injectable()
 export class ClubService {
-  test() {
-    return "hello";
+  constructor(
+    private clubRepository: ClubRepository
+  ) {}
+
+  public async getClubInfo(name: string): Promise<ClubInfoResObj> {
+    const clubInfo: ClubInfoResObj = await this.clubRepository.getClubInfo(name);
+    if(!clubInfo) {
+      throw new BadRequestException("Not Fount Club Info");
+    }
+    clubInfo.member = await this.clubRepository.getClubMemberAll(name);
+    return clubInfo;
   }
 }
