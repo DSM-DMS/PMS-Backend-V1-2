c01659c579e99e05890d2ea19aa750f3cf8e9386
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-23 20:26:34 +0900

    [MODIFY] <meal.scheduler.ts> Change Local time set

--- src/event/meal/meal.scheduler.ts
+++ src/event/meal/meal.scheduler.ts
@@ -19,7 +19,7 @@ async function setNewMeal() {
 }
 
 export async function setSchedule() {
-  const scheduleBreakfast = schedule.scheduleJob("0 0 10 * * *", setNewMeal);
-  const scheduleLunch = schedule.scheduleJob("0 0 14 * * *", setNewMeal);
-  const sheduleDinner = schedule.scheduleJob("0 0 19 * * *", setNewMeal);
+  const scheduleBreakfast = schedule.scheduleJob("0 0 1 * * *", setNewMeal);
+  const scheduleLunch = schedule.scheduleJob("0 0 5 * * *", setNewMeal);
+  const sheduleDinner = schedule.scheduleJob("0 0 10 * * *", setNewMeal);
 }
