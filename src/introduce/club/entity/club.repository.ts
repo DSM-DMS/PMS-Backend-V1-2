acf6aa18c24e76bd043e755931522a2dd7e416a0
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-23 10:36:07 +0900

    [MODIFY] <club.repository.ts> Fix typeorm builder

--- src/introduce/club/entity/club.repository.ts
+++ src/introduce/club/entity/club.repository.ts
@@ -6,21 +6,21 @@ import { Club } from "./club.entity";
 export class ClubRepository extends Repository<Club> {
   public getClubInfo(name: string): Promise<Club> {
     return this.createQueryBuilder("club")
-    .select("title")
-    .addSelect(`CONCAT('${process.env.DDYZD_URL}/file/', uri)`, "uri")
-    .addSelect("explanation")
-    .where("title = :name", { name })
+    .select("club.title")
+    .addSelect(`CONCAT('${process.env.DDYZD_URL}/file/', club.uri)`, "uri")
+    .addSelect("club.explanation")
+    .where("club.title = :name", { name })
     .getRawOne();
   }
 
   public async getClubMemberAll(club_name: string): Promise<string[]> {
     const members = await this.createQueryBuilder("club")
-    .select("title")
+    .select("club.title")
     .addSelect("GROUP_CONCAT(user.name)", "members")
     .leftJoin("club.club_member", "club_member")
     .leftJoin("club_member.user", "user")
-    .groupBy("title")
-    .having("title = :club_name", { club_name })
+    .groupBy("club.title")
+    .having("club.title = :club_name", { club_name })
     .getRawOne();
     return members.members.split(",");
   }
@@ -28,7 +28,7 @@ export class ClubRepository extends Repository<Club> {
   public getClubList(): Promise<ClubList[]> {
     return this.createQueryBuilder("club")
     .select("club.title", "club-name")
-    .addSelect("club.uri", "picture-uri")
+    .addSelect(`CONCAT('${process.env.DDYZD_URL}/file/', club.uri)`, "picture-uri")
     .getRawMany();
   }
 }
