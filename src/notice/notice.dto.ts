8a5da52af1b778877016fe2aa49c2c14595624e5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-25 16:38:25 +0900

    [ADD] <notice.entity.ts> Add notice model

--- src/notice/notice.dto.ts
+++ src/notice/notice.dto.ts
@@ -1,7 +1,7 @@
 import { Comment } from "./comment/comment.entity";
 
 export class NoticeInfoResObj {
-  "update-date": Date;
+  "upload-date": Date;
   title: string;
   body: string;
   attach: string[];
