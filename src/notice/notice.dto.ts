dda9ea53023dad44fb1db858caac1da2d92e3533
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-25 16:27:00 +0900

    [MODIFY] <notice.dto.ts>

--- src/notice/notice.dto.ts
+++ src/notice/notice.dto.ts
@@ -1,7 +1,9 @@
+import { Comment } from "./comment/comment.entity";
+
 export class NoticeInfoResObj {
   "update-date": Date;
   title: string;
   body: string;
   attach: string[];
-  comment: string[];
+  comment: Comment[];
 }
