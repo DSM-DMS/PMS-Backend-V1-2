79b0da5ea75a6a05832180dd214812509e7a81df
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-01 18:46:27 +0900

    [MODIFY] <notice.module.ts> Add TypeOrm Module

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NoticeRepository } from './entity/notice.repository';
 import { NoticeController } from './notice.controller';
 import { NoticeService } from './notice.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([NoticeRepository])],
   controllers: [NoticeController],
   providers: [NoticeService]
 })
