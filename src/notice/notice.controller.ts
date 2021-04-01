a84e9089789415f145261da594d406a91226e3a0
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-01 18:48:04 +0900

    [MODIFY] <notice.controller.ts> Add Swagger Api

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { ApiTags, ApiBearerAuth, ApiHeader, ApiOperation, ApiResponse, ApiConsumes, ApiBody } from "@nestjs/swagger";
+import { AuthGuard } from "src/shared/authentication/auth.guard";
+import { NoticeInfoResObj } from "./notice.dto";
+import { NoticeService } from "./notice.service";
 
+@ApiTags("notice")
+@ApiBearerAuth()
+@ApiHeader({ name: "Authorization", required: true })
 @Controller('notice')
-export class NoticeController {}
+export class NoticeController {
+  constructor(private noticeService: NoticeService) {}
+
+  @Get("/")
+  @UseGuards(new AuthGuard())
+  @ApiOperation({ summary: "공지사항 목록 API", description: "성공 시 상태 코드 200 반환" })
+  @ApiResponse({ status: 200, type: NoticeInfoResObj })
+  @ApiResponse({ status: 400, description: "잘못된 요청. 요청 값 확인" })
+  @ApiResponse({ status: 401, description: "인증 정보가 유효하지 않음" })
+  @ApiResponse({ status: 403, description: "접근 권한 없음" })
+  getNoticeList() {
+    return this.noticeService.getNoticeList();
+  }
+}
