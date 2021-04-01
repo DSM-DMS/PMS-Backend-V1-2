0f2f1e5f8010e28776ff14d5580b8624d9fe7fc4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-01 15:25:32 +0900

    [REFACTOR] <notice.crawl.ts> Add try catch Handler

--- src/notice/notice.crawl.ts
+++ src/notice/notice.crawl.ts
@@ -1,3 +1,4 @@
+import { Logger } from "@nestjs/common";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { DataParser } from "../shared/parser/parser.type";
@@ -16,7 +17,11 @@ export class NoticeCrawlDataParser extends DataParser<NoticeCrawlData> {
   }
 
   private getNoticeTitle(domContent: any): string {
-    return domContent.childNodes[4].childNodes[1].data;
+    try {
+      return domContent.childNodes[4].childNodes[1].data;
+    } catch(err) {
+      Logger.error(err);
+    }
   }
 
   private subStringNoticeBody(arr: any[]): void {
@@ -25,13 +30,17 @@ export class NoticeCrawlDataParser extends DataParser<NoticeCrawlData> {
   }
 
   private getNoticeContentMedia(domContent: any): string[] {
-    const media: string[] = [];
-    for(let i=1;; i+= 4) {
-      const medium = domContent.childNodes[8] ? domContent.childNodes[8].childNodes[1].childNodes[3].childNodes[i] : false;
-      if(!medium) break;
-      media.push(`${this.DSMHS_URL}${medium.attribs.href}`)
+    try {
+      const media: string[] = [];
+      for(let i=1;; i+= 4) {
+        const medium = domContent.childNodes[8] ? domContent.childNodes[8].childNodes[1].childNodes[3].childNodes[i] : false;
+        if(!medium) break;
+        media.push(`${this.DSMHS_URL}${medium.attribs.href}`)
+      }
+      return media;
+    } catch(err) {
+      Logger.error(err);
     }
-    return media;
   }
 
   public static getInstance(): NoticeCrawlDataParser {
@@ -39,28 +48,36 @@ export class NoticeCrawlDataParser extends DataParser<NoticeCrawlData> {
   }
 
   public async getParsingData(): Promise<NoticeCrawlData> {
-    const { data: pageHtml } = await axios.get("https://dsmhs.djsch.kr/boardCnts/list.do?type=default&page=1&m=0201&s=dsmhs&boardID=54793");
-    let $: any = cheerio.load(pageHtml);
-    const boardSeq: string = $(".link")[1].childNodes[1].attribs.onclick.toString().match(/[0-9]+/g)[1];
-    const { data: noticeHtml } = await axios.get(`${this.DSMHS_URL}/boardCnts/view.do?boardID=54793&boardSeq=${boardSeq}&lev=0&searchType=null&statusYN=W&page=3&pSize=11&s=dsmhs&m=0201&opType=N`);
-    $ = cheerio.load(noticeHtml);
-    const boardText: any = $(".board-text")[0];
-    const title: string = this.getNoticeTitle(boardText);
-    this.subStringNoticeBody(boardText.childNodes[6].children);
-    const body: string = this.noticeBody;
-    this.noticeBody = "";
-    const attach: string[] = this.getNoticeContentMedia(boardText);
-    return { body, title, attach };
+    try {
+      const { data: pageHtml } = await axios.get("https://dsmhs.djsch.kr/boardCnts/list.do?type=default&page=1&m=0201&s=dsmhs&boardID=54793");
+      let $: any = cheerio.load(pageHtml);
+      const boardSeq: string = $(".link")[1].childNodes[1].attribs.onclick.toString().match(/[0-9]+/g)[1];
+      const { data: noticeHtml } = await axios.get(`${this.DSMHS_URL}/boardCnts/view.do?boardID=54793&boardSeq=${boardSeq}&lev=0&searchType=null&statusYN=W&page=3&pSize=11&s=dsmhs&m=0201&opType=N`);
+      $ = cheerio.load(noticeHtml);
+      const boardText: any = $(".board-text")[0];
+      const title: string = this.getNoticeTitle(boardText);
+      this.subStringNoticeBody(boardText.childNodes[6].children);
+      const body: string = this.noticeBody;
+      this.noticeBody = "";
+      const attach: string[] = this.getNoticeContentMedia(boardText);
+      return { body, title, attach };
+    } catch(err) {
+      Logger.error(err);
+    }
   }
 
   public async setParsingData(data: NoticeCrawlData) {
-    await NoticeRepository.getQueryRepository().createQueryBuilder()
-    .insert()
-    .values([{
-      title: data.title,
-      body: data.body,
-      "upload-date": new Date(this.getUploadDate()),
-    }])
-    .execute();
+    try {
+      await NoticeRepository.getQueryRepository().createQueryBuilder()
+      .insert()
+      .values([{
+        title: data.title,
+        body: data.body,
+        "upload-date": new Date(this.getUploadDate()),
+      }])
+      .execute();
+    } catch(err) {
+      Logger.error(err);
+    }
   }
 }
