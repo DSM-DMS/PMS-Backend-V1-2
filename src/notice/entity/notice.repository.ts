4f9be51a1507008009c7ec175e2fd5a22bf5cd12
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-17 23:48:11 +0900

    [MODIFY] <notice.repository.ts> add get notice list method

--- src/notice/entity/notice.repository.ts
+++ src/notice/entity/notice.repository.ts
@@ -1,9 +1,18 @@
 import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import { NoticeInfoResObj } from "../dto/notice.dto";
 import { Notice } from "./notice.entity";
 
 @EntityRepository(Notice)
 export class NoticeRepository extends Repository<Notice> {
   static getQueryRepository() {
     return getCustomRepository(NoticeRepository);
+  }
+
+  public getNoticeList(): Promise<Notice[]> {
+    return this.createQueryBuilder("notice")
+    .select("notice.id")
+    .addSelect("notice.update-date")
+    .addSelect("notice.title")
+    .getMany();
   }
 }
