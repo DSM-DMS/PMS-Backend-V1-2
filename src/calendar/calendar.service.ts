47f2cd902a978ecf240d5456549ccc4a46618326
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-17 15:20:52 +0900

    [MODIFY] <calendar.parser.ts, calendar.service.ts> schedule method

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -8,10 +8,10 @@ export class CalendarService {
   private calendar: CalendarDataFactory;
   constructor() {
     this.calendar = new CalendarDataFactory();
-    schedule.scheduleJob("0 0 0 1 * *", this.calendar.setCalendar.bind(this.calendar));
+    schedule.scheduleJob("0 0 0 1 * *", this.calendar.setParsingData.bind(this.calendar));
   }
 
   public async getCalendar(): Promise<Calendar> {
-    return await this.calendar.getCalender();
+    return await this.calendar.getParsingData();
   }
 }
