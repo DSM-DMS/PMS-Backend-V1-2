a40d527cf72ccb847dd62f7477791793679f103f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-18 14:06:40 +0900

    [MODIFY] <notice.dto.ts, notice.repository.ts> add notice res object

--- src/notice/entity/notice.repository.ts
+++ src/notice/entity/notice.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, getCustomRepository, Repository } from "typeorm";
-import { NoticeInfoResObj } from "../dto/notice.dto";
+import { NoticeResObj } from "../dto/notice.dto";
 import { Notice } from "./notice.entity";
 
 @EntityRepository(Notice)
@@ -8,10 +8,10 @@ export class NoticeRepository extends Repository<Notice> {
     return getCustomRepository(NoticeRepository);
   }
 
-  public getNoticeList(): Promise<Notice[]> {
+  public getNoticeList(): Promise<NoticeResObj[]> {
     return this.createQueryBuilder("notice")
     .select("notice.id")
-    .addSelect("notice.update-date")
+    .addSelect("notice.upload-date")
     .addSelect("notice.title")
     .getMany();
   }
