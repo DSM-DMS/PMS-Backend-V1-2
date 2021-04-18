476b5148927021a20c09e143e3eff9199aec5bca
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-18 16:06:47 +0900

    [MODIFY] <notice.repository.ts> add get notice info method

--- src/notice/entity/notice.repository.ts
+++ src/notice/entity/notice.repository.ts
@@ -15,4 +15,24 @@ export class NoticeRepository extends Repository<Notice> {
     .addSelect("notice.title")
     .getMany();
   }
+
+  public getNoticeInfo(notice_id: number): Promise<Notice> {
+    return this.createQueryBuilder("notice")
+    .select("notice.id")
+    .addSelect("notice.upload-date")
+    .addSelect("notice.title")
+    .addSelect("notice.body")
+    .addSelect("attach.file_name")
+    .addSelect("comment.id")
+    .addSelect("comment.upload-date")
+    .addSelect("comment.body")
+    .addSelect("user.email")
+    .addSelect("user.name")
+    .addSelect("user.user_role")
+    .leftJoin("notice.attach", "attach")
+    .leftJoin("notice.comment", "comment")
+    .leftJoin("comment.user", "user")
+    .where("notice.id = :id", { id: notice_id })
+    .getOne();
+  }
 }
