b9d55c0a5382652a399fa35f7933fa2be4c95667
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-18 16:07:40 +0900

    [MODIFY] <notice.controller.ts, notice.module.ts> get notice info api

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
-import { ApiTags, ApiBearerAuth, ApiHeader, ApiOperation, ApiResponse, ApiConsumes, ApiBody } from "@nestjs/swagger";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { ApiTags, ApiBearerAuth, ApiHeader, ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { AuthGuard } from "../shared/authentication/auth.guard";
-import { NoticeResObj } from "./dto/notice.dto";
+import { NoticeInfoResObj, NoticeResObj } from "./dto/notice.dto";
 import { NoticeService } from "./notice.service";
 
 @ApiTags("notice")
@@ -20,5 +20,16 @@ export class NoticeController {
   @ApiResponse({ status: 403, description: "접근 권한 없음" })
   getNoticeList() {
     return this.noticeService.getNoticeList();
+  }
+
+  @Get("/:notice_id")
+  @UseGuards(new AuthGuard())
+  @ApiOperation({ summary: "공지사항 정보 API", description: "성공 시 상태 코드 200 반환" })
+  @ApiResponse({ status: 200, type: NoticeInfoResObj })
+  @ApiResponse({ status: 400, description: "잘못된 요청. 요청 값 확인" })
+  @ApiResponse({ status: 401, description: "인증 정보가 유효하지 않음" })
+  @ApiResponse({ status: 403, description: "접근 권한 없음" })
+  getNoticeInfo(@Param("notice_id") notice_id: number) {
+    return this.noticeService.getNoticeInfo(notice_id);
   }
 }
