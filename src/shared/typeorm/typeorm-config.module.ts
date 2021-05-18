1d3de44377d941eadcc07f3a6430ad33f70fe225
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-18 10:26:37 +0900

    [MOVE] <ormconfig.ts> typeorm module 분리

---
+++ src/shared/typeorm/typeorm-config.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { connectionOptions } from "./ormconfig";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV]),
+    TypeOrmModule.forRoot(connectionOptions["dms"]),
+    TypeOrmModule.forRoot(connectionOptions["ddyzd"]),
+  ]
+})
+export class TypeOrmConfigModule {}
