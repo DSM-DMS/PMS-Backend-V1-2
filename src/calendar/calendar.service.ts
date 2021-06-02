c44024e8eda7ec9849580c62559e7e638cb93515
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-02 22:57:31 +0900

    [REFACTORING] <calendar.ts, calendar.service.ts>

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -1,20 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { CalendarDataFactory } from "./parser/calendar";
 import { Calendar } from "./type/calendar.type";
-import * as schedule from "node-schedule";
 
 @Injectable()
 export class CalendarService {
   private calendar: CalendarDataFactory;
   constructor() {
     this.calendar = new CalendarDataFactory();
-    schedule.scheduleJob(
-      "0 0 0 1 * *",
-      this.calendar.setParsingData.bind(this.calendar),
-    );
   }
 
-  public async getCalendar(): Promise<Calendar> {
-    return await this.calendar.getParsingData();
+  public getCalendar(): Promise<Calendar> {
+    return this.calendar.getParsingData();
   }
 }
