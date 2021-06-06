c5188bbdd26491ecde535b9b89d715fffd007674
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-06 23:22:12 +0900

    [MODIFY] <parent.service.interface.ts> findUser 메소드

--- src/shared/parent/interface/parent.service.interface.ts
+++ src/shared/parent/interface/parent.service.interface.ts
@@ -1,3 +1,7 @@
+import { Parent } from "./entity/parent.entity.interface";
+
 export interface ParentService {
   checkAdminUserEmail(email: string): Promise<boolean>;
+  checkAdminUser(user: Parent): boolean;
+  findUser(email: string): Promise<Parent>;
 }
