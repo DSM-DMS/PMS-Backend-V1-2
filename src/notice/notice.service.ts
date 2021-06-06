4df46fff0aa880e26c6762ffce15ee641effb0f4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-06 17:08:56 +0900

    [MODIFY] <notice.service.ts> 변경사항 반영

--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { NoticeRepository } from "./entity/notice.repository";
 import { Notice } from "./entity/notice.entity";
-import { Comment } from "./comment/comment.entity";
 import { CommentRepository } from "./comment/comment.repository";
 import { NoticeListResponse } from "./dto/response/notice-list.response";
 import { NoticeInfoResponse } from "./dto/response/notice-info.response";
@@ -20,12 +19,13 @@ export class NoticeService {
 
   public async getNoticeInfo(notice_id: number): Promise<NoticeInfoResponse> {
     const notice: Notice = await this.noticeRepository.findById(notice_id);
-    await Promise.all(
-      notice.comment.map((comment: Comment) => {
-        return this.setLargeComments(comment);
-      }),
-    );
-    return notice;
+    if (!notice) {
+      throw new BadRequestException("Not Found Notice");
+    }
+    return {
+      ...notice,
+      comment: await this.commentRepository.findAllByNotice(notice_id),
+    };
   }
 
   public async getNoticeBySearch(
@@ -45,19 +45,5 @@ export class NoticeService {
     page: number,
   ): Promise<NoticeListResponse[]> {
     return this.noticeRepository.findByNewsKeyword(keyword, page);
-  }
-
-  private async setLargeComments(comment: Comment) {
-    const largeComments: Comment[] = await this.commentRepository.getLargeComment(
-      comment.id,
-    );
-    if (largeComments) {
-      comment.comment = largeComments;
-      await Promise.all(
-        comment.comment.map((comment: Comment) => {
-          return this.setLargeComments(comment);
-        }),
-      );
-    }
   }
 }
