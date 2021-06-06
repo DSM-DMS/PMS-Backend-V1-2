b7c8efd3e552a71bbcb989cf106c489c13b325c2
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-06 23:27:27 +0900

    [MODIFY] <notice.module.ts, notice.service.ts> 댓글작성 서비스

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,12 +1,16 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ParentModule } from "../shared/parent/parent.module";
 import { CommentRepository } from "./comment/comment.repository";
 import { NoticeRepository } from "./entity/notice.repository";
 import { NoticeController } from "./notice.controller";
 import { NoticeService } from "./notice.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NoticeRepository, CommentRepository])],
+  imports: [
+    TypeOrmModule.forFeature([NoticeRepository, CommentRepository]),
+    ParentModule,
+  ],
   controllers: [NoticeController],
   providers: [NoticeService],
 })
--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  ForbiddenException,
+  Inject,
+  Injectable,
+} from "@nestjs/common";
 import { Notice } from "./entity/notice.entity";
 import { Comment } from "./comment/comment.entity";
 import { NoticeRepository } from "./entity/notice.repository";
@@ -6,12 +11,17 @@ import { CommentRepository } from "./comment/comment.repository";
 import { NoticeListResponse } from "./dto/response/notice-list.response";
 import { NoticeInfoResponse } from "./dto/response/notice-info.response";
 import { NoticeCommentResponse } from "./dto/response/notice-comment.response";
+import { NoticeCommentRequest } from "./dto/request/notice-comment.request";
+import { Parent, ParentService } from "../shared/parent/interface";
+import { PARENT_SERVICE_TOKEN } from "../shared/parent/parent.module";
 
 @Injectable()
 export class NoticeService {
   constructor(
     private noticeRepository: NoticeRepository,
     private commentRepository: CommentRepository,
+    @Inject(PARENT_SERVICE_TOKEN)
+    private userService: ParentService,
   ) {}
 
   // 공지사항
@@ -57,5 +67,41 @@ export class NoticeService {
       throw new BadRequestException("Not Found Comment");
     }
     return this.commentRepository.findAllLargeComment(comment);
+  }
+
+  public async addNoticeComment(
+    body: NoticeCommentRequest,
+    notice_id: number,
+    email: string,
+  ): Promise<Comment> {
+    const findNoticePromise: Promise<Notice> = this.noticeRepository.findOne(
+      notice_id,
+    );
+    const findUserPromise: Promise<Parent> = this.userService.findUser(email);
+    const notice: Notice = await findNoticePromise;
+    const user: Parent = await findUserPromise;
+    let parent_comment: Comment = null;
+    if (!notice) {
+      throw new BadRequestException("Not Found Notice");
+    }
+    if (this.userService.checkAdminUser(user)) {
+      throw new ForbiddenException("Fobidden user");
+    }
+    if (body.comment_id) {
+      const exComment: Comment = await this.commentRepository.findOne(
+        body.comment_id,
+      );
+      if (!exComment) {
+        throw new BadRequestException("Not Found Comment");
+      }
+      parent_comment = exComment;
+    }
+    const comment: Comment = new Comment();
+    comment.user = user;
+    comment.notice = notice;
+    comment.body = body.body;
+    comment.parent_comment = parent_comment;
+    comment.depth = parent_comment ? parent_comment.depth + 1 : 0;
+    return comment.save();
   }
 }
