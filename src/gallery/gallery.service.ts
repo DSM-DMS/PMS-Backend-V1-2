27ed5bbe5bf5209eadd1ee8c51dcd8f2fb82077f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-07-07 22:43:33 +0900

    [MODIFY] <gallery.service.ts> getGalleryList

--- src/gallery/gallery.service.ts
+++ src/gallery/gallery.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { GalleryListResponse } from "./dto/response/gallery-list.response";
+import { Gallery } from "./entity/gallery.entity";
+import { GalleryRepository } from "./entity/gallery.repository";
 
 @Injectable()
-export class GalleryService {}
+export class GalleryService {
+  constructor(private galleryRepository: GalleryRepository) {}
+
+  public async getGalleryList(
+    page: number,
+    size: number,
+  ): Promise<GalleryListResponse[]> {
+    if (!size) {
+      throw new BadRequestException("Invalid Parameter");
+    }
+    const galleries: Gallery[] = await this.galleryRepository.findWithPageAndSize(
+      page,
+      size,
+    );
+    return galleries.map((gallery) => ({
+      ...gallery,
+      thumbnail: gallery.thumbnail.file_name,
+    }));
+  }
+}
