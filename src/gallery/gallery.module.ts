fb0e9cb73367d79dcd9cf3bf613c74812811016a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-07-13 19:23:54 +0900

    [MODIFY] <gallery.module.ts> image module import

--- src/gallery/gallery.module.ts
+++ src/gallery/gallery.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ImageModule } from "../shared/image/image.module";
 import { GalleryRepository } from "./entity/gallery.repository";
 import { GalleryController } from "./gallery.controller";
 import { GalleryService } from "./gallery.service";
+import { GalleryImageUplodaer } from "./uploader/gallery-image.uploader";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GalleryRepository])],
+  imports: [
+    TypeOrmModule.forFeature([GalleryRepository]),
+    ImageModule.register(GalleryImageUplodaer),
+  ],
   controllers: [GalleryController],
   providers: [GalleryService],
 })
