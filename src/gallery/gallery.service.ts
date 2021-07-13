cdf9b4144ecd8cb10b91681ccc1d492e84801ffd
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-07-13 19:24:57 +0900

    [MODIFY] <gallery.service.ts> image upload

--- src/gallery/gallery.service.ts
+++ src/gallery/gallery.service.ts
@@ -1,12 +1,26 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  Logger,
+} from "@nestjs/common";
+import { Connection, QueryRunner } from "typeorm";
+import { ImageModule } from "../shared/image/image.module";
 import { GalleryInfoResponse } from "./dto/response/gallery-info.response";
 import { GalleryListResponse } from "./dto/response/gallery-list.response";
 import { Gallery } from "./entity/gallery.entity";
 import { GalleryRepository } from "./entity/gallery.repository";
+import { GalleryImageUplodaer } from "./uploader/gallery-image.uploader";
 
 @Injectable()
 export class GalleryService {
-  constructor(private galleryRepository: GalleryRepository) {
+  constructor(
+    private galleryRepository: GalleryRepository,
+    private connection: Connection,
+
+    @Inject(ImageModule.LOCAL_IMAGE_UPLOADER)
+    private imageUploader: GalleryImageUplodaer,
+  ) {
     galleryRepository.find().then((g) => {
       this.galleryTotalLength = g.length;
     });
@@ -24,10 +38,12 @@ export class GalleryService {
     const galleries: Gallery[] =
       await this.galleryRepository.findWithPageAndSize();
     return {
-      galleries: galleries.splice(page * size, size).map((gallery: Gallery) => ({
-        ...gallery,
-        thumbnail: gallery.thumbnail.file_name,
-      })),
+      galleries: galleries
+        .splice(page * size, size)
+        .map((gallery: Gallery) => ({
+          ...gallery,
+          thumbnail: gallery.thumbnail.file_name,
+        })),
       totalLength: this.galleryTotalLength,
     };
   }
@@ -39,6 +55,11 @@ export class GalleryService {
     if (!gallery) {
       throw new BadRequestException("Invalid Parameter");
     }
+
+    gallery.attach[0] &&
+      /https:\/\/dsmhs\.djsch\.kr/g.test(gallery.attach[0].file_name) &&
+      this.uploadGalleryImage(gallery);
+
     return {
       gallery: {
         ...gallery,
@@ -46,5 +67,25 @@ export class GalleryService {
         thumbnail: gallery.thumbnail.file_name,
       },
     };
+  }
+
+  private async uploadGalleryImage(gallery: Gallery) {
+    const queryRunner: QueryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      for (const attach of gallery.attach) {
+        this.imageUploader.attach = attach;
+        this.imageUploader.entityManager = queryRunner.manager;
+        this.imageUploader.filePath = attach.file_name;
+        await this.imageUploader.toLocalFile();
+      }
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      Logger.error(err);
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
