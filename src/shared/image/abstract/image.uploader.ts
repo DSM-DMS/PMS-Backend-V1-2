0dc1ec82d32d0e8136f136b9918b441e498a6347
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-08-22 10:21:58 +0900

    [MODIFY] <image.uploader.ts> 파일 이름 uuid 통일

--- src/shared/image/abstract/image.uploader.ts
+++ src/shared/image/abstract/image.uploader.ts
@@ -1,30 +1,9 @@
 import { promisify } from "util";
 import * as request from "request";
 import * as fs from "fs";
+import { v4 } from "uuid";
 
 export abstract class ImageUploader {
-  private findLocalFileName(callback: (err: Error, filename: string) => void) {
-    const filePath: string = this.findDsmKrFilePath();
-    request(
-      filePath,
-      {
-        headers: {
-          "user-agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-          "Accept-Language": "ko,en;q=0.9,ko-KR;q=0.8,en-US;q=0.7",
-        },
-      },
-      (err, res) => {
-        err && callback(err, null);
-        const filename = decodeURIComponent(res.headers["content-disposition"])
-          .replace(/\"|filename|=|attachment;|;/g, "")
-          .trim()
-          .replace("jpg", "png");
-        callback(null, filename);
-      },
-    );
-  }
-
   private saveLocalFile(
     filename: string,
     callback: (err: Error, file_path: string) => void,
@@ -42,9 +21,7 @@ export abstract class ImageUploader {
   }
 
   public async toLocalFile() {
-    const filename: string = await promisify(this.findLocalFileName).bind(
-      this,
-    )();
+    const filename: string = v4() + ".png";
     const fileLocation: string = await promisify(this.saveLocalFile).bind(this)(
       filename,
     );
