ac2e61cce4893b4fe1bd2794e68744890977730a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-19 17:22:01 +0900

    [MODIFY] <typeorm-config.module.ts>

--- src/shared/typeorm/typeorm-config.module.ts
+++ src/shared/typeorm/typeorm-config.module.ts
@@ -6,7 +6,7 @@ import { connectionOptions } from "./ormconfig";
   imports: [
     TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV]),
     TypeOrmModule.forRoot(connectionOptions["dms"]),
-    //TypeOrmModule.forRoot(connectionOptions["ddyzd"]),
+    TypeOrmModule.forRoot(connectionOptions["ddyzd"]),
   ],
 })
 export class TypeOrmConfigModule {}
